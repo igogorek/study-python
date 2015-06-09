b934912e14ffc6adf7ba4f67bd505b5fe24a91dd
igogorek <forbib@yandex.ru>
2015-06-10 01:38:38 +0300

    letter_frequency one line solution

--- codewars/letter_frequency.py
+++ codewars/letter_frequency.py
@@ -1,14 +1,10 @@
 # return a list of tuples sorted by frequency with
 # the most frequent letter first. Any letters with the
 # same frequency are ordered alphabetically
-from collections import defaultdict
+from collections import Counter
 
 def letter_frequency(text):
 
-    letter_dict = defaultdict(int)
-    for letter in [symbol.lower() for symbol in text if symbol.isalpha()]:
-        letter_dict[letter] += 1
-
-    return sorted(letter_dict.items(), key=lambda x: (-x[1], x[0]))
+    return sorted(Counter(filter(str.isalpha, text.lower())).most_common(), key=lambda x: (-x[1], x[0]))
 
 print letter_frequency("As long as I'm learning something, I figure I'm OK - it's a decent day.")
