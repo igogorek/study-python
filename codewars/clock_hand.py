03302fd52907382a0483baf1c1388d2728ed2fdf
igogorek <forbib@yandex.ru>
2015-06-10 00:47:54 +0300

    clock_hands final solution

--- codewars/clock_hand.py
+++ codewars/clock_hand.py
@@ -1,19 +1,25 @@
 import datetime
 
+DEGREES_IN_CIRCLE = 360.0
+SECONDS_IN_HOUR = 60.0 * 60.0
+HOUR_HAND_SPEED = DEGREES_IN_CIRCLE / (SECONDS_IN_HOUR * 12.0)
+MINUTE_HAND_SPEED = DEGREES_IN_CIRCLE / SECONDS_IN_HOUR
+
 
 def clock_hands(angle):
-    def clock_hands_generator(angle):
-        hour_hand_speed = 360.0 / (3600.0 * 12.0)
-        minute_hand_speed = 360.0 / 3600.0
+    def time_gen(angle):
         hour = 0
         while hour < 11:
             yield datetime.datetime.utcfromtimestamp(
-                int((angle + hour * 3600 * minute_hand_speed) / (minute_hand_speed - hour_hand_speed)))
+                # angle = second_of_hour * MINUTE_HAND_SPEED - second_of_day * HOUR_HAND_SPEED
+                # second_of_hour = second_of_day - hour * SECONDS_IN_HOUR , then second_of_day is
+                (angle + hour * SECONDS_IN_HOUR * MINUTE_HAND_SPEED) / (MINUTE_HAND_SPEED - HOUR_HAND_SPEED))
             hour += 1
 
-    return ['{:0>2}:{:0>2}:{:0>2}'.format((12 - (12 - a_time.hour) % 12), a_time.minute, a_time.second)
-            for a_time in clock_hands_generator(angle)]
+    return ['{:0>2}:{:0>2}:{:0>2}'.format(t.hour or 12, t.minute, t.second) for t in time_gen(angle % 360)]
 
 
-print clock_hands(270)
+print clock_hands(120)
+
+
 
